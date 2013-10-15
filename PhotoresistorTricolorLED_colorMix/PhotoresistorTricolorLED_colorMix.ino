7e9b21224fca008cbfae940348ffbc3611ebc688
Abey Mani Ubuntu <abey_mail@yahoo.com>
2013-10-15 19:37:07 -0400

    adding photo resitor tri color led

---
+++ PhotoresistorTricolorLED_colorMix/PhotoresistorTricolorLED_colorMix.ino
@@ -0,0 +1,19 @@
+const int GREEN_LED_PIN = 9;
+const int RED_LED_PIN = 10;
+const int BLUE_LED_PIN = 11;
+
+const int RED_SENSOR_PIN = A0;
+const int GREEN_SENSOR_PIN = A1;
+const int BLUE_SENSOR_PIN = A2;
+
+int redValue = 0;
+
+
+void setup()
+{
+}
+
+void loop()
+{
+}
+
