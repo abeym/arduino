37bf9e16d9b615238042017a77356bfeea52909f
Abey Mani Ubuntu <abey_mail@yahoo.com>
2013-10-15 20:13:51 -0400

    tri color LED

--- PhotoresistorTricolorLED_colorMix/PhotoresistorTricolorLED_colorMix.ino
+++ PhotoresistorTricolorLED_colorMix/PhotoresistorTricolorLED_colorMix.ino
@@ -7,13 +7,41 @@ const int GREEN_SENSOR_PIN = A1;
 const int BLUE_SENSOR_PIN = A2;
 
 int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
 
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
 
 void setup()
 {
+  Serial.begin(9600);
+  pinMode(GREEN_LED_PIN, OUTPUT);  
+  pinMode(RED_LED_PIN, OUTPUT);  
+  pinMode(BLUE_LED_PIN, OUTPUT);  
 }
 
 void loop()
 {
+  redSensorValue = analogRead(RED_SENSOR_PIN);
+  greenSensorValue = analogRead(GREEN_SENSOR_PIN);
+  blueSensorValue = analogRead(BLUE_SENSOR_PIN);
+  
+  Serial.print("Raw Senor Value \t Red:");
+  Serial.print(redSensorValue);
+  Serial.print("\t Green:");
+  Serial.print(greenSensorValue);
+  Serial.print("\t Blue:");
+  Serial.print(blueSensorValue);
+  
+  redValue = redSensorValue / 4;
+  greenValue = greenSensorValue / 4;
+  blueValue = blueSensorValue / 4;
+
+  analogWrite(RED_LED_PIN, redValue);
+  analogWrite(GREEN_LED_PIN, greenValue);
+  analogWrite(BLUE_LED_PIN, blueValue);
+
 }
 
